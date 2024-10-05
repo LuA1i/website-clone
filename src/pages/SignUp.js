0818145efc3d9a038386b6b55e65ada362be564a
Luqman Ali <luqman.ali270304@outlook.com>
2024-10-05 21:55:14 +0100

    SignUp Page

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,7 +1,19 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import '../pages/details.css'
 const SignUp = () => {
-  return <div>singUp</div>
+  return (
+    <div className="form-container">
+      <h1>Sign Up</h1>
+      <Link>Have an Account</Link>
+      <form action="">
+        <input type="text" placeholder="Username" />
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Username" />
+      </form>
+      <button className='signUp-btn'>SignUp</button>
+    </div>
+  )
 }
 
 export default SignUp
